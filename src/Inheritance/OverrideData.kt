671a4bc61895b2b5f52c04a6574ed08e4d9d108e
SatishParmar1 <satishparmarparmar486@gmail.com>
2025-11-05 17:18:09 +0530

    2

--- src/Inheritance/OverrideData.kt
+++ src/Inheritance/OverrideData.kt
@@ -1,8 +1,27 @@
 package Inheritance
 
-class OverrideData {
+open class OverrideData {
     var id :Int = 1
     var name :String = "Shyam"
 
+    open fun printData(id1: Int, name1:String){
+        this.id = id1
+        this.name = name1
+        println("This is my Id $id")
+        println("This is my $name")
+    }
+}
 
+class OverrideData2 : OverrideData(){
+    override fun printData(id1: Int,name1:String){
+        this.id = id1
+        this.name = name1
+        println("not my $id")
+        println("not my $name")
+    }
+}
+
+fun main(){
+    var data = OverrideData2()
+    data.printData(2,"ram")
 }
